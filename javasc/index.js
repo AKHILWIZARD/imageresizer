e7a2027cb782fe1ef9ee57fa99b86b7d93e1f742
AKHILWIZARD <arunakhil978@gmail.com>
2023-10-18 20:54:22 +0530

    edit_code

--- javasc/index.js
+++ javasc/index.js
@@ -15,12 +15,11 @@ $(document).ready(function(){
 
         image.css('width', widthInPixels + 'px');
         image.css('height', imageheight1 + 'px');
-        image.css('height', heightpixel + '%');
-        image.css('width', percentage + '%');
+        image.attr('height', heightpixel + '%');
+        image.attr('width', percentage + '%');
 
-        link.attr('target', openInNewTab);
-        link.attr('href',linkInput)
-
+        link.attr('target', openInNewTab ? '_blank' : '');
+        link.attr('href',linkInput);
     });
 });
 
